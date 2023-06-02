c3b0fa3b682989a88e6655779bd6cc6bdb52a7d0
ibrahim3999 <mgdlhb@gmail.com>
2023-06-02 19:36:35 +0300

    DONE TESTS

--- Test.cpp
+++ Test.cpp
@@ -135,4 +135,119 @@ TEST_SUITE("MagicalContainer Tests") {
 
         CHECK(container.size() == 0);
     }
+    TEST_CASE("Add and Remove Elements") {
+    MagicalContainer container;
+
+    CHECK(container.size() == 0);
+
+    CHECK_THROWS(container.removeElement(25)); // Check if removeElement throws an exception on an empty container
+
+    container.addElement(2);
+    container.addElement(25);
+    container.addElement(3);
+    container.addElement(17);
+    container.addElement(9);
+
+    CHECK(container.size() == 5);
+
+    SUBCASE("Ascending Order") {
+        vector<int> expected = {2, 3, 9, 17, 25};
+        vector<int> actual = container.getElements();
+
+        CHECK(actual == expected);
+    }
+
+    SUBCASE("Cross Order") {
+        vector<int> expected = {2, 25, 17, 9, 3};
+
+        MagicalContainer::SideCrossIterator crossIter(container);
+        vector<int> actual;
+        for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+            actual.push_back(*it);
+        }
+
+        CHECK(actual == expected);
+    }
+
+    SUBCASE("Prime Numbers") {
+        vector<int> expected = {2, 3, 17};
+
+        MagicalContainer::PrimeIterator primeIter(container);
+        vector<int> actual;
+        for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+            actual.push_back(*it);
+        }
+
+        CHECK(actual == expected);
+    }
+
+    container.removeElement(25);
+    container.removeElement(9);
+
+    CHECK(container.size() == 3);
+
+    SUBCASE("Ascending Order After Removal") {
+        vector<int> expected = {2, 3, 17};
+        vector<int> actual = container.getElements();
+
+        CHECK(actual == expected);
+    }
+}TEST_CASE("Add and Remove Elements") {
+    MagicalContainer container;
+
+    CHECK(container.size() == 0);
+
+    CHECK_THROWS(container.removeElement(25)); // Check if removeElement throws an exception on an empty container
+
+    container.addElement(2);
+    container.addElement(25);
+    container.addElement(3);
+    container.addElement(17);
+    container.addElement(9);
+
+    CHECK(container.size() == 5);
+
+    SUBCASE("Ascending Order") {
+        vector<int> expected = {2, 3, 9, 17, 25};
+        vector<int> actual = container.getElements();
+
+        CHECK(actual == expected);
+    }
+
+    SUBCASE("Cross Order") {
+        vector<int> expected = {2, 25, 17, 9, 3};
+
+        MagicalContainer::SideCrossIterator crossIter(container);
+        vector<int> actual;
+        for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+            actual.push_back(*it);
+        }
+
+        CHECK(actual == expected);
+    }
+
+    SUBCASE("Prime Numbers") {
+        vector<int> expected = {2, 3, 17};
+
+        MagicalContainer::PrimeIterator primeIter(container);
+        vector<int> actual;
+        for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+            actual.push_back(*it);
+        }
+
+        CHECK(actual == expected);
+    }
+
+    container.removeElement(25);
+    container.removeElement(9);
+
+    CHECK(container.size() == 3);
+
+    SUBCASE("Ascending Order After Removal") {
+        vector<int> expected = {2, 3, 17};
+        vector<int> actual = container.getElements();
+
+        CHECK(actual == expected);
+    }
+}
 }
